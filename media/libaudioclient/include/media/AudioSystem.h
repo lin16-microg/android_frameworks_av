ae03486602b789cfb41b3b87841c0de9bd0318fa
Richard Folke Tullberg <richardfolke.tullberg@sony.com>
2017-01-13 09:04:25 +0100

    [BACKPORT] Add master audio balance
    Test: Change Balance through Settings, play audio
    Bug: 28390736
    Co-author: Ed Savage-Jones <edward.savage-jones@sony.com>
    Change-Id: I0169b436ccbaa5628584d9f4954dd7c76d021aae

--- media/libaudioclient/include/media/AudioSystem.h
+++ media/libaudioclient/include/media/AudioSystem.h
@@ -338,6 +338,9 @@ public:
     static status_t setMasterMono(bool mono);
     static status_t getMasterMono(bool *mono);
 
+    static status_t setMasterBalance(float balance);
+    static status_t getMasterBalance(float *balance);
+
     static float    getStreamVolumeDB(
             audio_stream_type_t stream, int index, audio_devices_t device);
 
